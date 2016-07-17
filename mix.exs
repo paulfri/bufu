1ab263b8d7387dd0cb33bb4d2f75a286b864b4cf
Paul Friedman <paulrfri@gmail.com>
2016-07-17 13:33:42 -0400

    Prepare for initial release

--- mix.exs
+++ mix.exs
@@ -3,31 +3,39 @@ defmodule Bufu.Mixfile do
 
   def project do
     [app: :bufu,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      elixirc_paths: elixirc_paths(Mix.env),
-     deps: deps]
+     deps: deps,
+     description: description,
+     package: package]
   end
 
   def application do
-    [
-      applications: [
-        :logger,
-        :httpotion
-      ]
-    ]
+    [applications: [:httpotion]]
   end
 
   defp deps do
-    [
-      {:credo, "~> 0.4", only: [:dev, :test]},
-      {:ex_doc, "~> 0.12", only: :dev},
-      {:exvcr, "~> 0.7", only: :test},
-      {:httpotion, "~> 3.0"},
-      {:poison, "~> 2.2"}
-    ]
+    [{:credo, "~> 0.4", only: [:dev, :test]},
+     {:ex_doc, "~> 0.12", only: :dev},
+     {:exvcr, "~> 0.7", only: :test},
+     {:httpotion, "~> 3.0"},
+     {:poison, "~> 2.2"}]
+  end
+
+  defp description do
+  """
+  Giant Bomb API client library.
+  """
+  end
+
+  defp package do
+  [name: "bufu",
+   licenses: ["MIT"],
+   maintainers: ["Paul Friedman"],
+   links: %{"GitHub" => "https://github.com/paulfri/bufu"}]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/helpers"]
